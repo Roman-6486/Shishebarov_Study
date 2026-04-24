1a646bbbbc2a2668669bbe64281c7fcc18d79e82
Roman-6486 <lypin6486@gmail.com>
2026-04-24 17:45:50 +0300

    Initial commit: Lab1 with Computer class

---
+++ Lab1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Computer
+    {
+        public int InventoryNumber;
+        public double CpuFrequency;
+        public int HardDiskSize;
+
+        public void Show()
+        {
+            Console.WriteLine($"Инвентарный номер: {InventoryNumber}");
+            Console.WriteLine($"Частота процессора: {CpuFrequency} ГГц");
+            Console.WriteLine($"Объем жесткого диска: {HardDiskSize} ГБ");
+        }
+
+        public double CalculateMemoryCost(double pricePerGB)
+        {
+            return HardDiskSize * pricePerGB;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Computer myComputer = new Computer();
+            myComputer.InventoryNumber = 123345;
+            myComputer.CpuFrequency = 1.5;
+            myComputer.HardDiskSize = 400;
+            myComputer.Show();
+            double totalCost = myComputer.CalculateMemoryCost(100);
+            Console.WriteLine($"Стоимость жесткого диска: {totalCost} руб.");
+            Console.ReadKey();
+        }
+    }
+}
